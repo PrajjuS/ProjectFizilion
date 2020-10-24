e8b2cbdafffed0ee345d109f280e3bdcc7d1be75
Shéikh Adnan <manofuranium@gmail.com>
2020-10-24 08:27:56 +0530

    log

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,11 +26,11 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running One4uBot [v3.7]")
+LOGS.info("You are running Project Fizilion")
 
 LOGS.info(
     "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
-    "If you need assistance, head to https://t.me/userbotindo")
+    "If you need assistance, head to https://t.me/ProjectFizilion")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
