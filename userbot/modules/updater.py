d95495302980010eb6690f112901f12971958ba4
Shéikh Adnan <manofuranium@gmail.com>
2020-11-06 12:52:11 +0530

    Mispelled

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -199,7 +199,7 @@ async def upstream(event):
             remove("output.txt")
         else:
             await event.edit(changelog_str)
-        return await event.respond('`do ".update now/deploy" to update`')
+        return await event.respond('`do ".ota now/deploy" to update`')
 
     if force_update:
         await event.edit(
