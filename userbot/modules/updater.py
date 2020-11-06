9a810674a59bdd007bd77fa59f2fd995225f1363
Shéikh Adnan <manofuranium@gmail.com>
2020-11-06 16:11:50 +0530

    Updater Ui Spacing

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -35,7 +35,7 @@ async def gen_chlog(repo, diff):
     ch_log = ''
     d_form = "%d/%m/%y"
     for c in repo.iter_commits(diff):
-        ch_log += f'{c.summary} \nby {c.author} \non [{c.committed_datetime.strftime(d_form)}]\n'
+        ch_log += f'\n{c.summary} \nby {c.author} \non [{c.committed_datetime.strftime(d_form)}]\n'
     return ch_log
 
 
