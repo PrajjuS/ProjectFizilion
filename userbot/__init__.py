b6502e1255dc312e4bbe49adb55ff94e95655001
Shéikh Adnan <manofuranium@gmail.com>
2020-11-09 13:12:25 +0530

    Fixed Mistake

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -132,7 +132,7 @@ ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT") or "False")
 ALIVE_NAME = os.environ.get("ALIVE_NAME") or None
 
 # Default .alive logo
-ALIVE_LOGO = str(os.environ.get("ALIVE_LOGO") or "https://ghttps://github.com/ElytrA8/ProjectFizilion/raw/dragon/resources/IMG_20201109_130207_262.jpg")
+ALIVE_LOGO = str(os.environ.get("ALIVE_LOGO") or "https://github.com/ElytrA8/ProjectFizilion/raw/dragon/resources/IMG_20201109_130207_262.jpg")
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY") or "")
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER") or 1)
