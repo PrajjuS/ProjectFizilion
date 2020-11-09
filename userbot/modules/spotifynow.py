7196a4b9ad64e38001fe0ed6b0251f68e3e6e53d
Shéikh Adnan <manofuranium@gmail.com>
2020-11-09 07:52:50 +0530

    .

--- userbot/modules/spotifynow.py
+++ userbot/modules/spotifynow.py
@@ -1,3 +1,10 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+# The entire source code is OSSRPL except 'makeqr and getqr' which is MPL
+# License: MPL and OSSRPL
 # Ported by Aidil Aryanto
 
 import os
