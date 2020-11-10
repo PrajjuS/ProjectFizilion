e672476ad61083ac2537addbaf88f97824a52a8c
PrajwalS3 <70487183+PrajwalS3@users.noreply.github.com>
2020-11-10 17:15:12 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -229,8 +229,7 @@ async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
-        "`Fizilion is Up and Running `\n"
-        f"`[Permium Edition] `\n"
+        "`Fizilion is Up [Premium] `\n"
         f"•`Telethon       : v{version.__version__} `\n"
         f"•`Python         : v{python_version()} `\n"
         f"`============================== `\n"
