af301965c4f856ee890060dfc175b3cd9e42b2ba
PrajwalS3 <70487183+PrajwalS3@users.noreply.github.com>
2020-11-12 18:20:18 +0530

    Update evaluators.py

--- userbot/modules/evaluators.py
+++ userbot/modules/evaluators.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot module for executing code and terminal commands from Telegram. """
@@ -9,26 +9,23 @@ import asyncio
 from os import remove
 from sys import executable
 
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, USER_TERM_ALIAS
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, TERM_ALIAS
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.eval(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.eval(?: |$|\n)(.*)")
 async def evaluate(query):
     """ For .eval command, evaluates the given Python expression. """
     if query.is_channel and not query.is_group:
-        await query.edit("`Eval isn't permitted on channels`")
-        return
+        return await query.edit("`Eval isn't permitted on channels`")
 
     if query.pattern_match.group(1):
         expression = query.pattern_match.group(1)
     else:
-        await query.edit("``` Give an expression to evaluate. ```")
-        return
+        return await query.edit("``` Give an expression to evaluate. ```")
 
     if expression in ("userbot.session", "config.env"):
-        await query.edit("`That's a dangerous operation! Not Permitted!`")
-        return
+        return await query.edit("`That's a dangerous operation! Not Permitted!`")
 
     try:
         evaluation = str(eval(expression))
@@ -66,37 +63,40 @@ async def evaluate(query):
 
     if BOTLOG:
         await query.client.send_message(
-            BOTLOG_CHATID, f"Eval query {expression} was executed successfully"
+            BOTLOG_CHATID, f"Eval query {expression} was executed successfully."
         )
 
 
-@register(outgoing=True, pattern=r"^.exec(?: |$)([\s\S]*)")
+@register(outgoing=True, pattern=r"^\.exec(?: |$|\n)([\s\S]*)")
 async def run(run_q):
     """ For .exec command, which executes the dynamically created program """
     code = run_q.pattern_match.group(1)
 
     if run_q.is_channel and not run_q.is_group:
-        await run_q.edit("`Exec isn't permitted on channels!`")
-        return
+        return await run_q.edit("`Exec isn't permitted on channels!`")
 
     if not code:
-        await run_q.edit(
-            "``` At least a variable is required to \
-execute. Use .help exec for an example.```"
+        return await run_q.edit(
+            "``` At least a variable is required to"
+            "execute. Use .help exec for an example.```"
         )
-        return
 
     if code in ("userbot.session", "config.env"):
-        await run_q.edit("`That's a dangerous operation! Not Permitted!`")
-        return
+        return await run_q.edit("`That's a dangerous operation! Not Permitted!`")
 
     if len(code.splitlines()) <= 5:
         codepre = code
     else:
         clines = code.splitlines()
         codepre = (
-            clines[0] + "\n" + clines[1] + "\n" + clines[2] + "\n" + clines[3] + "..."
-        )
+            clines[0] +
+            "\n" +
+            clines[1] +
+            "\n" +
+            clines[2] +
+            "\n" +
+            clines[3] +
+            "...")
 
     command = "".join(f"\n {l}" for l in code.split("\n.strip()"))
     process = await asyncio.create_subprocess_exec(
@@ -127,19 +127,19 @@ execute. Use .help exec for an example.```"
         )
     else:
         await run_q.edit(
-            "**Query: **\n`" f"{codepre}" "`\n**Result: **\n`No Result Returned/False`"
+            "**Query: **\n`" f"{codepre}" "`\n**Result: **\n`No result returned/False`"
         )
 
     if BOTLOG:
         await run_q.client.send_message(
-            BOTLOG_CHATID, "Exec query " + codepre + " was executed successfully"
+            BOTLOG_CHATID, "Exec query " + codepre + " was executed successfully."
         )
 
 
-@register(outgoing=True, pattern="^.term(?: |$)(.*)")
+@register(outgoing=True, pattern=r"^\.term(?: |$|\n)(.*)")
 async def terminal_runner(term):
     """ For .term command, runs bash commands and scripts on your server. """
-    curruser = USER_TERM_ALIAS
+    curruser = TERM_ALIAS
     command = term.pattern_match.group(1)
     try:
         from os import geteuid
@@ -149,19 +149,15 @@ async def terminal_runner(term):
         uid = "This ain't it chief!"
 
     if term.is_channel and not term.is_group:
-        await term.edit("`Term commands aren't permitted on channels!`")
-        return
+        return await term.edit("`Term commands aren't permitted on channels!`")
 
     if not command:
-        await term.edit(
-            "``` Give a command or use .help term for \
-            an example.```"
+        return await term.edit(
+            "``` Give a command or use .help term for an example.```"
         )
-        return
 
-    if command in ("userbot.session", "config.env", "env", "$", "$*", "echo *"):
-        await term.edit("`That's a dangerous operation! Not Permitted!`")
-        return
+    if command in ("userbot.session", "config.env"):
+        return await term.edit("`That's a dangerous operation! Not Permitted!`")
 
     process = await asyncio.create_subprocess_shell(
         command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
@@ -189,13 +185,14 @@ async def terminal_runner(term):
 
     if BOTLOG:
         await term.client.send_message(
-            BOTLOG_CHATID,
-            "Terminal Command " + command + " was executed sucessfully",
+            BOTLOG_CHATID, "Terminal command " + command + " was executed sucessfully.",
         )
 
 
-CMD_HELP.update({"eval": ".eval 2 + 3\nUsage: Evalute mini-expressions."})
-CMD_HELP.update({"exec": ".exec print('hello')\nUsage: Execute small python scripts."})
-CMD_HELP.update(
-    {"term": ".term ls\nUsage: Run bash commands and scripts on your server."}
-)
+CMD_HELP.update({"eval": ">`.eval 2 + 3`"
+                 "\nUsage: Evalute mini-expressions.",
+                 "exec": ">`.exec print('hello')`"
+                 "\nUsage: Execute small python scripts.",
+                 "term": ">`.term <cmd>`"
+                 "\nUsage: Run bash commands and scripts on your server.",
+                 })
