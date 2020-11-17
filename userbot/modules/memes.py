164da01613ffeceaebdf4aa37f9c4c3e7a0bd4d2
PrajwalS3 <70487183+PrajwalS3@users.noreply.github.com>
2020-11-17 09:41:44 +0530

    Update memes.py

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -1369,7 +1369,14 @@ async def nou(noway):
         await noway.edit(
           "`No U ultra pro max extremis supreme super duper hyper premium legendary epic mega omega expert maestro first class adept top-notch excellent magical super-magical superior exceptional dextrous ingenious gawd of gawds so pro af teach me how to be pro like you🔥`"
      )
-
+       
+@register(outgoing=True, pattern="^.pero$")
+async def nou(noway):
+    if not noway.text[0].isalpha() and noway.text[0] not in ("/", "#", "@", "!"):
+        await noway.edit(
+          "`U Pro AF ultra pro max extremis supreme super duper hyper premium legendary epic mega omega expert maestro first class adept top-notch excellent magical super-magical superior exceptional dextrous ingenious gawd of gawds`"
+     )
+        
 @register(outgoing=True, pattern="^.gm$")
 async def nou(noway):
     if not noway.text[0].isalpha() and noway.text[0] not in ("/", "#", "@", "!"):
@@ -1398,12 +1405,7 @@ async def nou(noway):
           "Good Night"
      )
 
-        @register(outgoing=True, pattern="^.pero$")
-async def nou(noway):
-    if not noway.text[0].isalpha() and noway.text[0] not in ("/", "#", "@", "!"):
-        await noway.edit(
-          "`U Pro AF ultra pro max extremis supreme super duper hyper premium legendary epic mega omega expert maestro first class adept top-notch excellent magical super-magical superior exceptional dextrous ingenious gawd of gawds`"
-     )
+
 
 CMD_HELP.update(
     {
