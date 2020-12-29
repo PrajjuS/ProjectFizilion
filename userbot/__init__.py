316bf0d430dd3f4c480c24f837e4eab3dcebe23f
Prajwal <70487183+PrajwalS3@users.noreply.github.com>
2020-12-29 21:19:25 +0530

    Changed chromium bin

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -111,7 +111,7 @@ REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY") or None
 
 # Chrome Driver and Headless Google Chrome Binaries
 CHROME_DRIVER = os.environ.get("CHROME_DRIVER") or None
-GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN") or None
+GOOGLE_CHROME_BIN = "/usr/bin/chromium"
 
 # OpenWeatherMap API Key
 OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID") or None
