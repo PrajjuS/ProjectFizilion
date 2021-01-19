3acbc330cd0a474b306057181e274fe3101c37cc
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-01-19 09:57:39 +0530

    Update memify2.py

--- userbot/modules/memify2.py
+++ userbot/modules/memify2.py
@@ -67,7 +67,7 @@ async def draw_meme_text(image_path, text):
     img = Image.open(image_path).convert("RGB")
     os.remove(image_path)
     i_width, i_height = img.size
-    m_font = ImageFont.truetype("resources/impact.ttf", int((70 / 640) * i_width))
+    m_font = ImageFont.truetype("resources/Aerospace.ttf", int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
     else:
