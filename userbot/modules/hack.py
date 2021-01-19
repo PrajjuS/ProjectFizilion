952222231fe67d4ad3b05e97ba30f21c7c7c933d
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-01-19 17:50:25 +0530

    Update hack.py

--- userbot/modules/hack.py
+++ userbot/modules/hack.py
@@ -43,6 +43,6 @@ async def hack_func(message):
               "To Remove This Hack```")
     max_ani = len(animation_chars)
     for i in range(max_ani):
-        await asyncio.sleep(2)
-        await message.edit(animation_chars[i % max_ani])
-    await message.edit(hecked)
+        await event.sleep(2)
+        await event.edit(animation_chars[i % max_ani])
+    await event.edit(hecked)
