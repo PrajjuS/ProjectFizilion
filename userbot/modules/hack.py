c62d13a67d4b23aa86a26fa163cf41907523425b
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-02-17 18:55:56 +0530

    Update hack.py

--- userbot/modules/hack.py
+++ userbot/modules/hack.py
@@ -2,7 +2,7 @@ from userbot.events import register
 from userbot import CMD_HELP
 import time
 from asyncio import sleep 
-from telethon import events , client
+from telethon import events , client , TelegramClient
 from userbot.modules.admin import get_user_from_event
 
 @register(outgoing=True, pattern="^.hack$")
