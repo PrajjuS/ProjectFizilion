4ff78c7097523eb2ed9f20c2e88ff923081abced
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-02-21 22:45:16 +0530

    Update sticklet.py

--- userbot/modules/sticklet.py
+++ userbot/modules/sticklet.py
@@ -45,7 +45,7 @@ async def sticklet(event):
     
     FONT_FILE = "/resources/Aerospace.ttf"
 
-    font = ImageFont.truetype(FONT_FILE, size=fontsize)
+    font = ImageFont(FONT_FILE, size=fontsize)
     
     await event.edit("Processing...........Kek")
     
