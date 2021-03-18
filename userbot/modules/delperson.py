6fb6eb3b67e49450d83fa1d7175f6e007d3bccb7
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-03-18 18:25:19 +0530

    Create delperson.py

---
+++ userbot/modules/delperson.py
@@ -0,0 +1,105 @@
+from asyncio import sleep
+from random import choice, randint
+
+from telethon.events import StopPropagation
+
+from userbot import (BOTLOG, BOTLOG_CHATID, CMD_HELP, COUNT_MSG,
+                     DELMSG, PM_AUTO_BAN, USERS)
+from userbot.events import register
+
+@register(incoming=True, disable_edited=True)
+async def on_dm(dom):
+    global COUNT_MSG
+    global USERS
+    global DELMSG
+    if dom.message.mentioned and DELMSG:
+        is_bot = False
+        if (sender := await mention.get_sender()):
+            is_bot = sender.bot
+        if not is_bot and mention.sender_id not in USERS:
+                USERS[mention.sender_id] = USERS[mention.sender_id] + 1
+        COUNT_MSG = COUNT_MSG + 1
+
+
+@register(incoming=True, disable_errors=True)
+async def delmsg_on_pm(sender):
+    global DELMSG
+    global USERS
+    global COUNT_MSG
+    if (
+        sender.is_private
+        and sender.sender_id != 777000
+        and not (await sender.get_sender()).bot
+    ):
+        if PM_AUTO_BAN:
+            try:
+                from userbot.modules.sql_helper.pm_permit_sql import \
+                    is_approved
+
+                apprv = is_approved(sender.sender_id)
+            except AttributeError:
+                apprv = True
+        else:
+            apprv = True
+                USERS[sender.sender_id] = USERS[sender.sender_id] + 1
+            COUNT_MSG = COUNT_MSG + 1
+
+
+@register(outgoing=True, pattern=r"^\.dm(?: |$)(.*)", disable_errors=True)
+async def set_delmsg(dm_e):
+    dm_e.text
+    string = dme_e.pattern_match.group(1)
+    global DELMSG
+        msg=await dm_e.edit("Turning on DELMSG")
+        await sleep(2)
+        awai t msg.delete()
+    if BOTLOG:
+        await afk_e.client.send_message(BOTLOG_CHATID, "#DELMSG\nYou turned on DELMSG!")
+    ISAFK = True
+    raise StopPropagation
+
+
+@register(outgoing=True)
+async def type_del_is_not_true(notdel):
+    global DELMSG
+    global COUNT_MSG
+    global USERS
+    if DELMSG:
+        DELMSG = False
+        msg = await notdel.respond("Delmsg turned off.")
+        await sleep(2)
+        await msg.delete()
+        if BOTLOG:
+            await del.client.send_message(
+                BOTLOG_CHATID,
+                "You've recieved "
+                + str(COUNT_MSG)
+                + " messages from "
+                + str(len(USERS))
+                + " chats while you were away",
+            )
+            for i in USERS:
+                name = await notdel.client.get_entity(i)
+                name0 = str(name.first_name)
+                await notdel.client.send_message(
+                    BOTLOG_CHATID,
+                    "["
+                    + name0
+                    + "](tg://user?id="
+                    + str(i)
+                    + ")"
+                    + " sent you "
+                    + "`"
+                    + str(USERS[i])
+                    + " messages`",
+                )
+        COUNT_MSG = 0
+        USERS = {}
+
+
+CMD_HELP.update(
+    {
+        "dm": ">`.dm`"
+        "\nUsage: Delete Msg history turns on. "
+    }
+)
