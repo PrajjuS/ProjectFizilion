40cec08b5b18568efd15c56edfd77e91e4cdfd69
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-03-19 19:09:51 +0530

    revert back

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -95,10 +95,6 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN") or None
 UPSTREAM_REPO_URL = (os.environ.get("UPSTREAM_REPO_URL")
                      or "https://github.com/PrajjuS/ProjectFizilion.git")
 UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH") or "Demon"
-#
-FUPSTREAM_REPO_URL = (os.environ.get("FUPSTREAM_REPO_URL")
-                     or "https://github.com/ElytrA8/ProjectFizilion.git")
-FUPSTREAM_REPO_BRANCH = os.environ.get("FUPSTREAM_REPO_BRANCH") or "dragon"
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get(
