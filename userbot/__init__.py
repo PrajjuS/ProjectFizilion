32026132f19da5af2c39e85cbc59f5541129f22b
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-08 19:01:38 +0530

    send alive message whenever bot comes online

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -242,33 +242,35 @@ with bot:
         quit(1)
 
 
-async def send_alive_status():
-    if BOTLOG_CHATID and LOGSPAMMER:
-        DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-        repo = Repo()
-        modules = CMD_HELP
-        uptime = await get_readable_time((time.time() - StartTime))
-        output = (
-            "` =============================== `\n"
-            f"`Fizilion is Up [Premium Edition] `\n"
-            f"`=============================== `\n"
-            f"•`Telethon       : v{version.__version__} `\n"
-            f"•`Python         : v{python_version()} `\n"   
-            f"•`User           : {DEFAULTUSER} `\n"
-            f"•`Running on     : {repo.active_branch.name} `\n"
-            f"•`Loaded modules : {len(modules)} `\n"
-            f"•`Fizilion       : {USERBOT_VERSION} `\n"
-            f"•`Bot Uptime     : {uptime} `\n"
-        )
-        await bot.send_message(BOTLOG_CHATID, output)
-        return True
-
+async def start():
+    if BOTLOG:
+        try:
+            DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+            repo = Repo()
+            modules = CMD_HELP
+            uptime = await get_readable_time((time.time() - StartTime))
+            output = (   
+                "` =============================== `\n"
+                f"`Fizilion is Up [Premium Edition] `\n"
+                f"`=============================== `\n"
+                f"•`Telethon       : v{version.__version__} `\n"
+                f"•`Python         : v{python_version()} `\n"  
+                f"•`User           : {DEFAULTUSER} `\n"
+                f"•`Running on     : {repo.active_branch.name} `\n"
+                f"•`Loaded modules : {len(modules)} `\n"
+                f"•`Fizilion       : {USERBOT_VERSION} `\n"
+                f"•`Bot Uptime     : {uptime} `\n"
+            )
+            await bot.send_message(
+                BOTLOG_CHATID, output
+                        )
+        except BaseException:
+            None
+    else:
+        pass
 
 with bot:
-    try:
-        bot.loop.run_until_complete(send_alive_status())
-    except:
-        pass
+    bot.loop.run_until_complete(start())
 
 
 # Global Variables
