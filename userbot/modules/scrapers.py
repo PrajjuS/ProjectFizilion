4e7a7246120431d7313a07bb0166f119825bd458
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-11 19:30:52 +0530

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -185,10 +185,6 @@ async def gsearch(event):
 
     try:
         gresults = await gsearch.async_search(*search_args)
-    except Exception:
-        return await event.edit(
-            "`Error: Your query could not be found or it was flagged as unusual traffic.`"
-        )
     msg = ""
 
     for i in range(counter):
