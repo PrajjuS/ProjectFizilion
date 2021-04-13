d1cba5a454abc2dc7b7e24a42a0123ec2301477c
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-13 12:40:38 +0530

    New Module: kill

---
+++ userbot/modules/kill.py
@@ -0,0 +1,28 @@
+import asyncio
+from asyncio import sleep
+
+
+
+@register(outgoing=True, pattern="^.kill$")
+async def kill_func(message):
+    animation_chars = [
+        "killing...",
+        "Ｆｉｉｉｉｉｒｅ",
+        "(　･ิω･ิ)︻デ═一-->",
+        "------>_____________",
+        "--------->___⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠⁠_______",
+        "-------------->_____",
+        "------------------->",
+        "------>;(^。^)ノ",
+        "(￣ー￣) DED",
+        "<b>Target killed successfully (´°̥̥̥̥̥̥̥̥ω°̥̥̥̥̥̥̥̥｀)</b>",
+    ]
+    for i in range(10):
+        await sleep(0.6)
+        await message.edit(animation_chars[i % 10], parse_mode="html")
+
+CMD_HELP.update({
+    "kill":
+    "'.kill'"
+    "\nUsage: Kill Meme"
+    })
