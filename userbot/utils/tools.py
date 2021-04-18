0c8e37bc89793310e5215bba6d32be335d049d3a
Senpai-sama-afk <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-04-18 16:24:23 +0000

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -1,13 +1,24 @@
-# Copyright (C) 2019 Adek Maulana
+ # Copyright (C) 2020 Adek Maulana
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
+# SPDX-License-Identifier: GPL-3.0-or-later
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import re
+
+import asyncio
 import hashlib
-
-from telethon.tl.types import DocumentAttributeFilename
+import re
+from typing import Union
 
 
 async def md5(fname: str) -> str:
@@ -18,11 +29,11 @@ async def md5(fname: str) -> str:
     return hash_md5.hexdigest()
 
 
-def humanbytes(size: int) -> str:
+def humanbytes(size: Union[int, float]) -> str:
     if size is None or isinstance(size, str):
         return ""
 
-    power = 2**10
+    power = 2 ** 10
     raised_to_pow = 0
     dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
     while size > power:
@@ -36,28 +47,33 @@ def time_formatter(seconds: int) -> str:
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     tmp = (
-        ((str(days) + " day(s), ") if days else "") +
-        ((str(hours) + " hour(s), ") if hours else "") +
-        ((str(minutes) + " minute(s), ") if minutes else "") +
-        ((str(seconds) + " second(s), ") if seconds else "")
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
     )
     return tmp[:-2]
 
 
 def human_to_bytes(size: str) -> int:
     units = {
-        "M": 2**20, "MB": 2**20,
-        "G": 2**30, "GB": 2**30,
-        "T": 2**40, "TB": 2**40
+        "M": 2 ** 20,
+        "MB": 2 ** 20,
+        "G": 2 ** 30,
+        "GB": 2 ** 30,
+        "T": 2 ** 40,
+        "TB": 2 ** 40,
     }
 
     size = size.upper()
-    if not re.match(r' ', size):
-        size = re.sub(r'([KMGT])', r' \1', size)
+    if not re.match(r" ", size):
+        size = re.sub(r"([KMGT])", r" \1", size)
     number, unit = [string.strip() for string in size.split()]
     return int(float(number) * units[unit])
 
-
+   
+   
+   
 async def check_media(reply_message):
     if reply_message and reply_message.media:
         if reply_message.photo:
@@ -80,11 +96,7 @@ async def check_media(reply_message):
             return False
     else:
         return False
-
-    if not data or data is None:
-        return False
-    else:
-        return data
+     
 async def run_cmd(cmd: list) -> tuple[bytes, bytes]:
     process = await asyncio.create_subprocess_exec(
         *cmd,
