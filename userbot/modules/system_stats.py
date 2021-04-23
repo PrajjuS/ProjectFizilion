e247eced4784c8d1fab5275b57830bd74509e389
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-04-23 22:28:18 +0530

    alive: delete alive msg after 15 secs

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -243,15 +243,19 @@ async def amireallyalive(alive):
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
-            await bot.send_file(alive.chat_id, logo, caption=output)
+            msg = await bot.send_file(alive.chat_id, logo, caption=output, del_in=10)
             await alive.delete()
+            await sleep(15)
+            await msg.delete()
         except BaseException:
             await alive.edit(
                 output + "\n\n *`The provided logo is invalid."
                 "\nMake sure the link is directed to the logo picture`"
             )
     else:
-        await alive.edit(output)
+        msg=await alive.edit(output)
+        await sleep(15)
+        await msg.delete()
 
 
 @register(outgoing=True, pattern="^.aliveu")
