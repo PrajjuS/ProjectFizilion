c184b1c11894d399443895db8fcf5c6e3e28f8f0
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-16 13:55:45 +0000

    Update deezload.py

--- userbot/modules/deezload.py
+++ userbot/modules/deezload.py
@@ -147,7 +147,7 @@ async def upload_track(track_location, message):
     caption_rts = os.path.basename(track_location)
     with open(track_location, "rb") as f:
         result = await upload_file(
-            client=client.event,
+            client=message.event,
             file=f,
             name=caption_rts
         )       
