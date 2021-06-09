9e57184a0f19e8204923baea85e6d3578486533d
Prajwal <70487183+PrajjuS@users.noreply.github.com>
2021-06-09 15:14:30 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+RUNDIR=${PWD}
+BOTDIR=$(dirname $(realpath ${0}))
+UNIXTIME=$(date +%s)
+
+if [ -z ${1} ] || [ ${1} == "-x" ] ; then
+	cd ${BOTDIR}
+	. venv/bin/activate
+	python3 -m userbot
+	RETVAL=${?}
+	deactivate
+	cd ${RUNDIR}
+	exit $RETVAL
+elif [ ${1} == "-d" ] || [ ${1} == "--daemonize" ] ; then
+	echo -e "I: Running silently in the background like a ninja..."
+	cd ${BOTDIR}
+	. venv/bin/activate
+	nohup python3 -m userbot >>${UNIXTIME}-fizilion.log 2>&1 & echo "I: Userbot PID is ${!}"
+	deactivate
+	cd ${RUNDIR}
+	exit 0
+elif [ ${1} == "-h" ] || [ ${1} == "--help" ] ; then
+	echo -e "ProjectFizilion"
+	echo -e  "Licensed under Raphielscape Public License"
+	echo -e "\nUsage:"
+	echo -e "[no arguments] || -x -- Run in the foreground, stdout is the logcat"
+	echo -e "--daemonize    || -d -- Run in the background, logs will be saved in *-fizilion.log"
+	exit 0
+else
+	echo -e  "ProjectFizilion"
+        echo -e  "Licensed under Raphielscape Public License"
+        echo -e  "\nUsage:"
+        echo -e  "[no arguments] || -x -- Run in the foreground, stdout is the logcat"
+        echo -e  "--daemonize    || -d -- Run in the background, logs will be saved in *-fizilion.log"
+	exit 127
