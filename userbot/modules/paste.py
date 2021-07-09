4a42b865132d6046c894148127b2c40b68975e97
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-07-09 21:04:28 +0400

    Update paste.py

--- userbot/modules/paste.py
+++ userbot/modules/paste.py
@@ -1,3 +1,6 @@
+##
+# Copyright (C) 2021 KenHV
+##IDK wat dis means
 from requests import post
 from telethon.tl.types import MessageMediaWebPage
 
@@ -7,8 +10,8 @@ from userbot.events import register
 
 @register(outgoing=True, pattern=r"^\.paste(?:\s|$)([\s\S]*)")
 async def paste(event):
-    """Pastes given text to Nekobin"""
-    await event.edit("**Pasting to Nekobin...**")
+    """Pastes given text to Katb.in"""
+    await event.edit("**Processing...**")
 
     if event.is_reply:
         reply = await event.get_reply_message()
@@ -22,16 +25,16 @@ async def paste(event):
     else:
         return await event.edit("**Read** `.help paste`**.**")
 
-    response = post("https://nekobin.com/api/documents", json={"content": message}).json()
+    response = post("https://api.katb.in/api/paste", json={"content": message}).json()
 
     if response["msg"] == "Successfully created paste":
         await event.edit(
-            f"**Pasted successfully:** [Nekobin](https://nekobin.com/raw/{response['paste_id']})\n"
+            f"**Pasted successfully:** [Katb.in](https://katb.in/{response['paste_id']})\n"
         )
     else:
-        await event.edit("**Nekobin seems to be down.**")
+        await event.edit("**Katb.in seems to be down.**")
 
 
 CMD_HELP.update(
-    {"paste": ">`.paste` <text/reply>" "\nUsage: Pastes given text to Nekobin."}
+    {"paste": ">`.paste` <text/reply>" "\nUsage: Pastes given text to Katb.in."}
 )
