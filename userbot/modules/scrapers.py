baa523bdf0b630e852c82e40a20a8f1dc42c52a0
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-07-09 21:34:51 +0400

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -471,50 +471,31 @@ async def imdb(e):
 
 
 @register(outgoing=True, pattern=r"^\.trt(?: |$)([\s\S]*)")
-async def translateme(trans):
-    """For .trt command, translate the given text using Google Translate."""
-
-    if trans.is_reply and not trans.pattern_match.group(1):
-        message = await trans.get_reply_message()
-        message = str(message.message)
+async def _(event):
+    if len(event.text) > 3:
+        if not event.text[3] == " ":
+            return
+    input = event.text[4:6]
+    txt = event.text[7:]
+    xx = await event.edit("`Translating...`")
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input or "en"
+    elif input:
+        text = txt
+        lan = input or "en"
     else:
-        message = str(trans.pattern_match.group(1))
-
-    if not message:
-        return await trans.edit(
-            "**Give some text or reply to a message to translate!**"
-        )
-
-    await trans.edit("**Processing...**")
+        return await xx.edit(f"`{hndlr}tr LanguageCode` as reply to a message", time=5)
     translator = Translator()
-    #translator = google_translator()
-
     try:
-        from userbot.modules.sql_helper.globals import gvarstatus
-    except AttributeError:
-        return await trans.edit("**Running on Non-SQL mode!**")
+        tt = translator.translate(text, dest=lan)
+        output_str = f"**TRANSLATED** from {tt.src} to {lan}\n{tt.text}"
+        await xx.edit(output_str)
+    except Exception as exc:
+        await xx.edit(str(exc), time=10)
 
-    if gvarstatus("trt_lang") is not None:
-        target_lang = str(gvarstatus("trt_lang"))
-    else:
-        target_lang = "en"
-
-    try:
-        reply_text = translator.translate(message, dest=target_lang)
-    except ValueError:
-        return await trans.edit(
-            "**Invalid language selected, use **`.lang trt <language code>`**.**"
-        )
-
-    try:
-        source_lan = translator.detect(deEmojify(message))[1].title()
-    except:
-        source_lan = "(Google didn't provide this info)"
-
-    reply_text = f"From: **{source_lan}**\nTo: **{LANGUAGES.get(target_lang).title()}**\n\n{reply_text}"
-
-    await trans.edit(reply_text)
-    
+        
 @register(pattern=r"\.lang (trt|tts) (.*)", outgoing=True)
 async def lang(value):
     """ For .lang command, change the default langauge of userbot scrapers. """
