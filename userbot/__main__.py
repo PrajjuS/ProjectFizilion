8a610a03a3cafa52940e9f61e516e4a47263738c
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-07-10 12:22:21 +0400

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,9 +7,9 @@
 
 from importlib import import_module
 from sys import argv
-
+from asyncio import sleep
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import LOGS, bot
+from userbot import LOGS, bot, HEROKU_APP_NAME, HEROKU_API_KEY
 from userbot.modules import ALL_MODULES
 
 
@@ -31,5 +31,9 @@ LOGS.info("You are running Project Fizilion")
 LOGS.info(
     "Congratulations, your userbot is now running !! Test it by typing .alive / .on in any chat."
     "If you need assistance, head to https://t.me/ProjectFizilionChat")
-
+if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
+    print("HEROKU detected, sleeping for 5 minutes to prevent String Session Error")
+    LOGS.info("HEROKU detected, sleeping for 5 minutes to prevent String Session Error")
+    await sleep(300)
+    bot.run_until_disconnected()
 bot.run_until_disconnected()
