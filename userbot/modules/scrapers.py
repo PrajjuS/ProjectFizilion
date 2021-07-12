585fe226c366dced1f408035aec87df0cb149203
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-07-12 14:50:34 +0400

    Translator API fix, ota deploy instead of ota now

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -2,7 +2,6 @@
 #
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-#
 """ Userbot module containing various scrapers. """
 
 import asyncio
